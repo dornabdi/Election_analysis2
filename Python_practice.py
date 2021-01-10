ca50d3a7d85f8f83387d64c2a82744050708e5d8
Dorna Abdi <dornaabdi@Dornas-MacBook-Pro.local>
2021-01-09 20:28:18 -0600

    Adding PyPoll.py file.

---
+++ Python_practice.py
@@ -0,0 +1,48 @@
+counties = ["Arapahoe", "Denver", "Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])
+
+
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
+
+if "Arapahoe" in counties or "El Paso" in counties:
+    print("Arapahoe or El Paso is in the list of counties.")
+else:
+    print("Arapahoe and El Paso are not in the list of counties.")
+
+for county in counties:
+    print(county)
+
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county in counties_dict:
+    print(county)
+
+for county in counties_dict:
+    print(counties_dict[county])
+
+for county, voters in counties_dict.items():
+    print(county, voters)
+
+
+for county in counties_dict:
+    print (f"{county} has {voters} registered voters.")
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    print(county_dict)
+
+
+candidate_votes = int(input("How many votes did the candidate get in the election? "))
+total_votes = int(input("What is the total number of votes in the election? "))
+message_to_candidate = (e
+    f"You received {candidate_votes:,} number of votes. "
+    f"The total number of votes in the election was {total_votes:,}. "
+    f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
