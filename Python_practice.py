768f2c81decf0701ef34d9f1752cc324c87eb21f
Dorna Abdi <dornaabdi@Dornas-MacBook-Pro.local>
2021-01-10 07:13:40 -0600

    Adding the election analysis

--- Python_practice.py
+++ Python_practice.py
@@ -43,6 +43,6 @@ for county_dict in voting_data:
 candidate_votes = int(input("How many votes did the candidate get in the election? "))
 total_votes = int(input("What is the total number of votes in the election? "))
 message_to_candidate = (e
-    f"You received {candidate_votes:,} number of votes. "
-    f"The total number of votes in the election was {total_votes:,}. "
+  #  f"You received {candidate_votes:,} number of votes. "
+  #  f"The total number of votes in the election was {total_votes:,}. "
     f"You received {candidate_votes / total_votes * 100:.2f}% of the total votes.")
